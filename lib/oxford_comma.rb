58745e9c85b65f83d79a9f7c5d19c3808a8c50b8
Maria Nunez <mariaalician@gmail.com>
2018-06-13 21:44:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 array = ["fiddleheads","okra", "fig"]
 def oxford_comma(array)
   if array.size == 2 
-    
-  result = array.join(" and ")
+  array.join(" and ")
+  elsif array.size == 1
+  array[0]
   array << result
   result
 end
