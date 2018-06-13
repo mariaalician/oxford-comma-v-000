f351867fb57d9b48dfb2c8875b405bd6aa4acd09
Maria Nunez <mariaalician@gmail.com>
2018-06-13 22:02:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+array = ["fiddleheads","okra", "fig"]
 def oxford_comma(array)
-
+  if array.size == 1
+  array[0]
+  elsif  array.size == 2 
+  array.join(" and ")
+  else
+    array[0..-2].join(", ") + ", and " + array.last
+  end
 end
